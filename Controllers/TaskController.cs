bba52fb454db92904ee0af575c6976292fb299aa
DavidObadovic <david.obadovic3@gmail.com>
2025-08-01 22:53:13 +0200

    first commit

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        private static List<TaskItem> lista = new List<TaskItem>();
+
+        [HttpGet]
+        public ActionResult<List<TaskItem>> GetTask()
+        {
+            return Ok(lista);
+        }
+
+        [HttpPost]
+        public ActionResult<List<Task>> CreateTask(TaskItem task)
+        {
+            task.Id = lista.Count + 1;
+            lista.Add(task);
+            return CreatedAtAction(nameof(GetTask), new { id = task.Id }, task);
+        }
+
+    }
+}
